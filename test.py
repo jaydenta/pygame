49b843efcaf7d688ce418f0c9a089c43e1e84dde
Jayden Ta <jaydenta8@gmail.com>
2018-07-15 14:04:41 -0700

    add comments to function

--- test.py
+++ test.py
@@ -2,11 +2,17 @@ import pytest
 from app import *
 
 def test_number():
+	'''
+	Check if f returns the sum of 2 number
+	'''
 	actual = f(1,2)
 	expect = 3
 	assert actual == expect
 
 def test_string():
+	'''
+	Check if f returns the combination of two strings
+	'''
 	actual = f('a','b')
 	expect = 'ab'
 	assert actual == expect
