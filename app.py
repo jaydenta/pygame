e21e1a67bc4aea44cc61db16bfa87218d6caee88
Jayden Ta <jaydenta8@gmail.com>
2018-07-15 14:13:24 -0700

    initial flask app

--- app.py
+++ app.py
@@ -1,5 +1,15 @@
+from flask import Flask
+app = Flask(__name__)
+
 def f(a,b):
 	"""
 	return the sum of 2 numbers or the combination of two strings
 	"""
 	return a+b
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+if __name__ == '__main__':
+   app.run(debug=True)
